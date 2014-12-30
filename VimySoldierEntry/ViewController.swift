cf53ddc80dee631bc3093eee8dffcc089f04783f
Mason Black <Blacky@Masons-iMac.local>
2014-12-30 18:16:08 -0500

    Initial Commit

---
+++ VimySoldierEntry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VimySoldierEntry
+//
+//  Created by Mason Black on 2014-12-30.
+//  Copyright (c) 2014 Mason Black. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
